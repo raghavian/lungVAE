6540072454b367c6bc0d154a0207a38970d62d2e
Raghav <raghav@di.ku.dk>
2020-05-31 10:45:05 +0200

    Zero padding for input

--- predict.py
+++ predict.py
@@ -22,20 +22,10 @@ from utils.tools import dice,binary_accuracy
 from torchvision.utils import save_image
 import os
 plt.gray()
-
-def makeOverlay(pred,mask):
-	mask = (mask > 0.5).float()
-	img = torch.zeros(mask.shape[0],3,mask.shape[2],mask.shape[3]).to(device)
-	pred =	((pred.detach() > 0.5).float() + 2*mask).squeeze()
-	img[0,0][pred == 1] = 0.55
-	img[0,2][pred == 2] = 0.6
-	img[0,1][pred == 3] = 0.75
-	img[(img.sum(1,True) + img) == 0] = 1.0
-
-	return img
 	
 def loadDCM(f, preprocess=False,dicom=False):
-	wLoc = 512
+	
+	wLoc = 448
 	### Load input dicom
 	if dicom:
 		dcm = dcmread(f).pixel_array
@@ -52,22 +42,22 @@ def loadDCM(f, preprocess=False,dicom=False):
 	
 	### Crop and resize image to 640x512 
 	hLoc = int((dcm.shape[0]/(dcm.shape[1]/wLoc)))
-	if hLoc > 640:
-		hLoc = 640
+	if hLoc > 576:
+		hLoc = 576
 		wLoc = int((dcm.shape[1]/(dcm.shape[0]/hLoc)))
 
 	img = resize(dcm,(hLoc,wLoc))
 	img = torch.Tensor(img)
 	pImg = torch.zeros((640,512))
-	h = (int((640-hLoc)/2))
-	w = int((512-wLoc)/2)
+	h = (int((576-hLoc)/2))+p
+	w = int((448-wLoc)/2)+p
 	roi = torch.zeros(pImg.shape)
-	if w == 0:
-		pImg[np.abs(h):(h+img.shape[0]),:] = img
-		roi[np.abs(h):(h+img.shape[0]),:] = 1.0
+	if w == p:
+		pImg[np.abs(h):(h+img.shape[0]),p:-p] = img
+		roi[np.abs(h):(h+img.shape[0]),p:-p] = 1.0
 	else:
-		pImg[:,np.abs(w):(w+img.shape[1])] = img	
-		roi[:,np.abs(w):(w+img.shape[1])] = 1.0
+		pImg[p:-p,np.abs(w):(w+img.shape[1])] = img	
+		roi[p:-p,np.abs(w):(w+img.shape[1])] = 1.0
 
 	imH = dcm.shape[0]
 	imW = dcm.shape[1]
@@ -79,24 +69,27 @@ def saveMask(f,img,h,w,hLoc,wLoc,imH,imgW,post=False):
 	img = img.data.numpy()
 	imgIp = img.copy()
 	
-	if w == 0:
-		img = resize(img[np.abs(h):(h+hLoc),:],(imH,imW),preserve_range=True)
+	if w == p:
+		img = resize(img[np.abs(h):(h+hLoc),p:-p],
+					(imH,imW),preserve_range=True)
 	else:
-		img = resize(img[:,np.abs(w):(w+wLoc)],(imH,imW),preserve_range=True)
+		img = resize(img[p:-p,np.abs(w):(w+wLoc)],
+					(imH,imW),preserve_range=True)
 	imsave(f,img_as_ubyte(img>0.5))
 
 	if post:
 		imgPost = postProcess(imgIp)
-		if w == 0:
-			imgPost = resize(imgPost[np.abs(h):(h+hLoc),:],(imH,imW))
+		if w == p:
+			imgPost = resize(imgPost[np.abs(h):(h+hLoc),p:-p],
+							(imH,imW))
 		else:
-			imgPost = resize(imgPost[:,np.abs(w):(w+wLoc)],(imH,imW))
+			imgPost = resize(imgPost[p:-p,np.abs(w):(w+wLoc)],
+							(imH,imW))
 
 		imsave(f.replace('.png','_post.png'),img_as_ubyte(imgPost > 0.5))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data', type=str, default='.', help='Path to input files')
-parser.add_argument('--label', type=str, default='.', help='Path to labels files')
 parser.add_argument('--model', type=str, default='.', help='Path to trained model')
 parser.add_argument('--hidden', type=int, default=16, help='Hidden units')
 parser.add_argument('--latent', type=int, default=8, help='Latent dim')
@@ -104,10 +97,13 @@ parser.add_argument('--saveLoc', type=str, default='', help='Path to save predic
 parser.add_argument('--unet',action='store_true', default=False,help='Use only U-Net.')
 parser.add_argument('--dicom',action='store_true', default=False,help='DICOM inputs.')
 parser.add_argument('--post',action='store_true', default=False,help='Post process predictions')
-parser.add_argument('--preprocess',action='store_true', default=False,help='Preprocess input images')
+parser.add_argument('--preprocess',action='store_true', 
+						default=False,help='Preprocess input images')
+parser.add_argument('--padding', type=int, default=32, help='Zero padding')
 
 
 args = parser.parse_args()
+p = args.padding
 print("Loading "+args.model)
 if 'unet' in args.model:
 	args.unet = True
@@ -124,7 +120,7 @@ t = time.strftime("%Y%m%d_%H_%M")
 if args.saveLoc is '':
 	save_dir = args.data+'pred_'+t+'/'
 else:
-	save_dir = args.saveLoc
+	save_dir = args.saveLoc+'pred_'+t+'/'
 if not os.path.exists(save_dir):
 	os.mkdir(save_dir)
 
@@ -146,7 +142,7 @@ for fIdx in range(len(files)):
 		fName = f.split('/')[-1]
 		img, roi, h, w, hLoc, wLoc, imH, imW = loadDCM(f,
 													preprocess=args.preprocess,
-													dcm=args.dicom)
+													dicom=args.dicom)
 		img = img.to(device)
 		_,mask = net(img)
 		mask = torch.sigmoid(mask*roi)
